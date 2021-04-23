c911705276fba9fa2ed58c5babb978980f0b8ac4
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-04-23 13:18:33 +0530

    Update Question_three.java

--- DataBase With Java/Database_myadmin/src/java/Question_three.java
+++ DataBase With Java/Database_myadmin/src/java/Question_three.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author asus
+ * @author Pratik Pathak
  */
 @WebServlet(urlPatterns = {"/Question_three"})
 public class Question_three extends HttpServlet {
