8694cd15068b454caf9b37c8ed9e8aa8f16addd6
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-07-08 18:58:53 +0530

    Create test.java

---
+++ Hibernate/mansikijayho/src/java/bean/test.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author asus
+ */
+public class test implements Serializable{
+    private int cust_id;
+
+    public int getCust_id() {
+        return cust_id;
+    }
+
+    public void setCust_id(int cust_id) {
+        this.cust_id = cust_id;
+    }
+    
+    
+}
